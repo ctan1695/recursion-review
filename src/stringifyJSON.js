fd572a53c54d5a5e7abf318da07a207998520830
Chloe Tan <chloe.tan95@gmail.com>
2020-10-16 21:28:32 -0400

    test

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,26 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  var result = [];
+  var str = '';
+  // base case
+  // number, boolean, null
+  if (typeof obj === null || typeof obj === undefined) {
+    return 'null';
+  }
+  if (typeof obj === 'number' ) { //|| typeof obj === 'boolean' || typeof obj === 'null'){
+    return obj + '';
+  } if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  } if (Array.isArray(obj)) { // recursive call
+    for ( var i = 0; i < obj.length; i++) {
+      //var current=stringifyJSON(obj[i])
+      result.push(obj[i]);
+      console.log('result', result);
+    }
+    str = '[' + result + ']';
+  }
+
+  // recursion call
+  return str;
 };
